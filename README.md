2c01d06a8c088e7ec1ef17b699d729bc4c52ee82
noodlefrenzy <noodlefrenzy@hotmail.com>
2014-10-08 22:10:18 -0700

    Add connection class, test for it, some more readme

--- README.md
+++ README.md
@@ -6,6 +6,39 @@ I've started a new project rather than fork from node-amqp or amqp.node.  Both n
 great 0.9.1 clients and I recommend them, but neither is pursuing a 1.0 implementation.  If I can find an
 easy way to integrate this code back into them, I'll definitely be submitting a PR.
 
+## Protocol Notes ##
+
+The [AMQP 1.0 Protocol](http://docs.oasis-open.org/amqp/core/v1.0/amqp-core-complete-v1.0.pdf) differs substantially 
+from the [0.9.1 protocol](http://www.rabbitmq.com/resources/specs/amqp0-9-1.pdf), with 0.9.1 defining exchanges, brokers, 
+types of queues (fanout, topic, etc.) and 1.0 focusing on more robust data interchange specifications and leaving much 
+of the server implementation to the implementor.  This section is just attempting to document features of this protocol 
+with the aim of enlightening those looking into the defined classes (and helping me drive my implementation).
+
++   Connection
+
+    An AMQP connection is a full-duplex ordered sequence of frames.
+    
++   Frame
+
+    A Frame is the bitstream defining a full parsable unit.
+    
++   Channel
+
+    A connection is divided into a negotiated number of independent unidirectional channels.  Frames are marked with
+    their parent channel number.
+    
++   Session
+
+    Correlation of two channels to form a bi-di sequential conversation.
+    
+    Connections may have multiple independent sessions active simultaneously, up to the negotiated channel limit.
+
++   Link
+    
+    Connection between two nodes.  Provides a credit-based flow-control scheme, each terminus of a link must track 
+    stream state.  Links are named, and may outlive their associated connections, allowing reconnection and retention
+    of associated state.
+    
 ## Caveats ##
 
 NOT YET IMPLEMENTED!  I'm just starting on this work - please ignore this repo for now.  Unless you want to help,
---
+++ test/test_connection.js
@@ -0,0 +1,60 @@
+var should      = require('should'),
+    Connection  = require('../lib/connection');
+
+describe('Connection', function() {
+    describe('#_parseAddress()', function() {
+
+        it('should match amqp(|s) no port no route', function () {
+            var conn = new Connection();
+
+            var addr = 'amqp://localhost/';
+            var result = conn._parseAddress(addr);
+            result.protocol.should.eql('amqp');
+            result.host.should.eql('localhost');
+            result.port.should.eql('5672');
+            result.path.should.eql('/');
+
+            addr = 'amqps://127.0.0.1';
+            result = conn._parseAddress(addr);
+            result.should.eql({
+                protocol: 'amqps',
+                host: '127.0.0.1',
+                port: '5671',
+                path: '/'
+            });
+        });
+
+        it('should match with port and with/without route', function() {
+            var conn = new Connection();
+
+            var addr = 'amqp://localhost:1234';
+            var result = conn._parseAddress(addr);
+            result.should.eql({
+                protocol: 'amqp',
+                host: 'localhost',
+                port: '1234',
+                path: '/'
+            });
+
+            addr = 'amqps://mq.myhost.com:1235/myroute?with=arguments&multiple=arguments';
+            result = conn._parseAddress(addr);
+            result.should.eql({
+                protocol: 'amqps',
+                host: 'mq.myhost.com',
+                port: '1235',
+                path: '/myroute?with=arguments&multiple=arguments'
+            });
+        });
+
+/*
+        it('should connect to activemq', function(done) {
+            var conn = new Connection();
+            conn.open('amqp://localhost/');
+            setTimeout(function() {
+                conn.close();
+                done();
+            }, 1000);
+        });
+*/
+    });
+});
