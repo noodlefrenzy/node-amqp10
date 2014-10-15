4aa6e34d8e60284506f35e985fbe2fa2755dd02d
Michael Lanzetta <lanzetta@gmail.com>
2014-10-14 18:18:48 -0700

    Adding coverage reports

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,8 @@ module.exports = function(grunt) {
               ui: 'bdd',
               reporter: 'list'
             },
-            all: { src: ['test/**/*.js'] }
+            all: { src: ['test/**/*.js'] },
+            coverage: { src: ['coverage/instrument/test/**/*.js' ] }
         },
 
         jshint: {
@@ -23,14 +24,48 @@ module.exports = function(grunt) {
                 src: ['amqp_client.js', 'lib/**/*.js'],
                 dest: 'api/README.md'
             }
+        },
+
+        env: {
+            coverage: {
+                DIR_FOR_CODE_COVERAGE: '../coverage/instrument/'
+            }
+        },
+        clean: {
+            coverage: {
+                src: [ 'coverage/instrument' ]
+            }
+        },
+        instrument: {
+            files: ['amqp_client.js', 'lib/**/*.js', 'test/**/*.js' ],
+            options: {
+                lazy: true,
+                basePath: 'coverage/instrument/'
+            }
+        },
+        storeCoverage: {
+            options: {
+                dir: 'coverage/reports'
+            }
+        },
+        makeReport: {
+            src: 'coverage/reports/**/*.json',
+            options: {
+                type: 'html',
+                dir: 'coverage/reports',
+                print: 'detail'
+            }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-simple-mocha');
     grunt.loadNpmTasks("grunt-jsdoc-to-markdown");
+    grunt.loadNpmTasks('grunt-istanbul');
 
-    grunt.registerTask('test', ['simplemocha']);
+    grunt.registerTask('coverage', ['instrument', 'simplemocha:coverage', 'storeCoverage', 'makeReport']);
+    grunt.registerTask('test', ['simplemocha:all']);
 
-    grunt.registerTask('default', ['jshint', 'test', 'jsdoc2md']);
+    grunt.registerTask('default', ['jshint', 'jsdoc2md', 'coverage']);
 };
