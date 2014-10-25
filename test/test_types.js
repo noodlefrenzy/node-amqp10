ded35db47e1efd3a379697e1277cb96ad02bf2a4
noodlefrenzy <noodlefrenzy@hotmail.com>
2014-10-25 11:03:44 -0700

    Adding doc-generator for frame performatives

--- test/test_types.js
+++ test/test_types.js
@@ -2,6 +2,8 @@ var should      = require('should'),
     debug       = require('debug')('amqp10-test-types'),
     builder     = require('buffer-builder'),
     Int64       = require('node-int64'),
+    xml2js      = require('xml2js'),
+    fs          = require('fs'),
 
     types       = require('../lib/types'),
     codec       = require('../lib/codec');
@@ -57,6 +59,98 @@ function assertDecoders(tests) {
         }
     }
 }
+
+function collapseDoc(prefix, docHead) {
+    var doc = prefix ? '<i>'+prefix+'</i>' : prefix;
+    if (docHead) {
+        for (var idx in docHead) {
+            var curDoc = docHead[idx];
+            if (curDoc.p) {
+                for (var pidx in curDoc.p) {
+                    var curP = curDoc.p[pidx];
+                    if (typeof(curP) === 'string') {
+                        doc += '\n' + curDoc.p[pidx];
+                    } else {
+                        if (curP._) {
+                            doc += '\n' + curP._;
+                        }
+                        debugger;
+                        if (curP.xref && curP.xref[0] && curP.xref[0].$ && curP.xref[0].$.name) {
+                            doc += '\n' + curP.xref[0].$.name;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return doc;
+}
+
+function buildDescriptor(descriptor) {
+    var doc = '';
+    if (descriptor) {
+        doc += '<h3>Descriptor</h3>\n';
+        doc += '<dl>\n<dt>Name</dt>\n<dd>'+descriptor[0].$.name+'</dd>\n<dt>Code</dt>\n<dd>'+descriptor[0].$.code+'</dd>\n</dl>\n';
+    }
+    return doc;
+}
+
+/*
+ {
+ "$": {
+ "name": "container-id",
+ "type": "string",
+ "mandatory": "true",
+ "label": "the id of the source container"
+ }
+ },
+
+ */
+function buildField(field) {
+    var doc = '<tr><td>'+field.$.name+'</td><td>'+field.$.type+'</td><td>'+(field.$.mandatory?'true':'false')+'</td></tr>\n';
+    var desc = collapseDoc(field.$.label, field.doc);
+    if (desc) {
+        doc += '<tr><td>&nbsp;</td><td colspan="2">'+desc+'</td></tr>\n';
+    }
+    return doc;
+}
+
+function toComment(doc) {
+    return '/**\n * ' + doc.replace(/\n/g, '\n * ') + '\n *\n * @constructor\n */';
+}
+
+describe('Gen', function () {
+    it('should gen performative docs', function() {
+        var parser = new xml2js.Parser();
+        var transportXml = fs.readFileSync('./resources/transport.xml');
+        parser.parseString(transportXml, function (err, res) {
+            var sections = res.amqp.section;
+            for (var idx=0; idx < sections.length; ++idx) {
+                if (sections[idx]['$'].name === 'performatives') {
+                    var performatives = sections[idx].type;
+                    fs.writeFileSync('./resources/transport.json', JSON.stringify(performatives, null, ' '));
+                    for (var pidx in performatives) {
+                        var performative = performatives[pidx];
+                        var name = performative['$'].name;
+                        var desc = collapseDoc(performative['$'].label, performative.doc);
+                        var descriptor = buildDescriptor(performative.descriptor);
+                        var fullDoc = '<h2>' + name + ' performative</h2>\n' + desc + '\n' + descriptor;
+                        if (performative.field) {
+                            var fields = '<table>\n';
+                            fields += '<tr><th>Name</th><th>Type</th><th>Mandatory?</th></tr>';
+                            for (var fidx in performative.field) {
+                                fields += buildField(performative.field[fidx]);
+                            }
+                            fields += '</table>';
+                            fullDoc += '\n' + fields;
+                        }
+                        fs.writeFileSync('./resources/'+name+'_doc.txt', toComment(fullDoc));
+                    }
+                }
+            }
+        });
+    });
+});
 
 describe('Types', function() {
     describe('#encoders', function() {
