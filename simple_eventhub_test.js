826ffe166da6474037622e108448fa59047e8a56
noodlefrenzy <noodlefrenzy@hotmail.com>
2015-03-02 13:17:29 -0800

    Modifying simple tests to allow sending or receiving.

--- simple_eventhub_test.js
+++ simple_eventhub_test.js
@@ -1,9 +1,5 @@
 var AMQPClient  = require('./amqp_client');
 
-var exceptions  = require('./lib/exceptions');
-
-var msgVal = Math.floor(Math.random() * 10000);
-
 var filterOffset; // example filter offset value might be: 43350;
 var filter;
 if (filterOffset) {
@@ -13,59 +9,98 @@ if (filterOffset) {
     };
 }
 
+function argCheck(settings, options) {
+    var missing = [];
+    for (var idx in options) {
+        if (settings[options[idx]] === undefined) missing.push(options[idx]);
+    }
+    if (missing.length > 0) {
+        throw new Error('Required settings ' + (missing.join(', ')) + ' missing.');
+    }
+}
+
 if (process.argv.length < 3) {
     console.warn('Usage: node '+process.argv[1]+' <settings json file>');
 } else {
     var settingsFile = process.argv[2];
     var settings = require('./' + settingsFile);
-    exceptions.assertArguments(settings, [ 'serviceBusHost', 'SASKeyName', 'SASKey', 'eventHubName', 'partitions' ]);
+    argCheck(settings, [ 'serviceBusHost', 'SASKeyName', 'SASKey', 'eventHubName', 'partitions' ]);
     var protocol = settings.protocol || 'amqps';
     var serviceBusHost = settings.serviceBusHost + '.servicebus.windows.net';
-    if (settings.serviceBusHost.indexOf(".") !== -1) {
-        serviceBusHost = settings.serviceBusHost;
-    }
     var sasName = settings.SASKeyName;
     var sasKey = settings.SASKey;
     var eventHubName = settings.eventHubName;
     var numPartitions = settings.partitions;
 
-    var msgVal = Math.floor(Math.random() * 10000);
-
     var uri = 'amqps://' + encodeURIComponent(sasName) + ':' + encodeURIComponent(sasKey) + '@' + serviceBusHost;
     var sendAddr = eventHubName;
     var recvAddr = eventHubName + '/ConsumerGroups/$default/Partitions/';
 
+    var msgVal = Math.floor(Math.random() * 1000000);
+
+    var sender = true;
+    var receiver = true;
+    if (process.argv.length > 3) {
+        if (process.argv[3] === 'send') receiver = false;
+        else if (process.argv[3] === 'receive') sender = false;
+        else throw new Error('Unknown action.');
+    }
+
+    var receiveCB = function (myIdx, err, payload, annotations) {
+        if (err) {
+            console.log('ERROR: ');
+            console.log(err);
+        } else {
+            console.log('Recv(' + myIdx + '): ');
+            console.log(payload);
+            if (annotations) {
+                console.log('Annotations:');
+                console.log(annotations);
+            }
+            console.log('');
+            if (sender) {
+                // If not a sender, no use checking for value.
+                if (payload.DataValue === msgVal) {
+                    client.disconnect(function () {
+                        console.log("Disconnected, when we saw the value we'd inserted.");
+                        process.exit(0);
+                    });
+                }
+            }
+        }
+    };
+
+    var sendCB = function(tx_err, state) {
+        if (tx_err) {
+            console.log('Error Sending: ');
+            console.log(tx_err);
+        } else {
+            if (receiver) {
+                for (var idx = 0; idx < numPartitions; ++idx) {
+                    var curIdx = idx;
+                    var curRcvAddr = recvAddr + curIdx;
+                    client.receive(curRcvAddr, filter, receiveCB.bind(null, curIdx));
+                }
+            } else {
+                console.log('Send message with value ' + msgVal + '.  Not receiving, so exiting');
+                process.exit(0);
+            }
+        }
+    };
+
     var client = new AMQPClient(AMQPClient.policies.EventHubPolicy);
     client.connect(uri, function() {
-        client.send({ "DataString": "From Node", "DataValue": msgVal }, sendAddr, { 'x-opt-partition-key' : 'pk1' }, function(tx_err, state) {
-            var receiveHandler = function (myIdx, err, payload, annotations) {
-                if (err) {
-                    console.log('ERROR: ');
-                    console.log(err);
-                } else {
-                    console.log('Recv(' + curIdx + '): ');
-                    console.log(payload);
-                    if (annotations) {
-                        console.log('Annotations:');
-                        console.log(annotations);
-                    }
-                    console.log('');
-                    if (payload.DataValue === msgVal) {
-                        client.disconnect(function () {
-                            if (state) {
-                                console.log('State from disposition: ', state);
-                            }
-                            console.log("Disconnected, when we saw the value we'd inserted.");
-                            process.exit(0);
-                        });
-                    }
-                }
-            };
+        if (sender) {
+            client.send({
+                "DataString": "From Node",
+                "DataValue": msgVal
+            }, sendAddr, {'x-opt-partition-key': 'pk' + msgVal}, sendCB);
+        } else if (receiver) {
             for (var idx = 0; idx < numPartitions; ++idx) {
                 var curIdx = idx;
                 var curRcvAddr = recvAddr + curIdx;
-                client.receive(curRcvAddr, filter, receiveHandler.bind(null, curIdx));
+                client.receive(curRcvAddr, undefined, receiveCB.bind(null, curIdx));
             }
-        });
+        }
     });
 }
--- simple_servicebus_queues.js
+++ simple_servicebus_queues.js
@@ -1,55 +1,83 @@
 var AMQPClient  = require('./amqp_client');
 
-var exceptions  = require('./lib/exceptions');
-
-var msgVal = Math.floor(Math.random() * 10000);
+function argCheck(settings, options) {
+    var missing = [];
+    for (var idx in options) {
+        if (settings[options[idx]] === undefined) missing.push(options[idx]);
+    }
+    if (missing.length > 0) {
+        throw new Error('Required settings ' + (missing.join(', ')) + ' missing.');
+    }
+}
 
 if (process.argv.length < 3) {
     console.warn('Usage: node '+process.argv[1]+' <settings json file>');
 } else {
     var settingsFile = process.argv[2];
     var settings = require('./' + settingsFile);
-    exceptions.assertArguments(settings, [ 'serviceBusHost', 'SASKeyName', 'SASKey', 'queueName']);
+    argCheck(settings, [ 'serviceBusHost', 'SASKeyName', 'SASKey', 'queueName']);
     var protocol = settings.protocol || 'amqps';
     var serviceBusHost = settings.serviceBusHost + '.servicebus.windows.net';
-    if (settings.serviceBusHost.indexOf(".") !== -1) {
-        serviceBusHost = settings.serviceBusHost;
-    }
     var sasName = settings.SASKeyName;
     var sasKey = settings.SASKey;
     var queueName = settings.queueName;
 
-    var msgVal = Math.floor(Math.random() * 10000);
+    var msgVal = Math.floor(Math.random() * 1000000);
 
     var uri = 'amqps://' + encodeURIComponent(sasName) + ':' + encodeURIComponent(sasKey) + '@' + serviceBusHost + '/' + queueName;
 
-    var client = new AMQPClient(AMQPClient.policies.ServiceBusQueuePolicy);
-    client.send({"DataString": "From Node", "DataValue": msgVal}, uri, function (tx_err, state) {
+    var sender = true;
+    var receiver = true;
+    if (process.argv.length > 3) {
+        if (process.argv[3] === 'send') receiver = false;
+        else if (process.argv[3] === 'receive') sender = false;
+        else throw new Error('Unknown action.');
+    }
+
+    var receiveCB = function (rx_err, payload, annotations) {
+        if (rx_err) {
+            console.log('Error Receiving: ');
+            console.log(rx_err);
+        } else {
+            console.log('Recv: ');
+            console.log(payload);
+            if (annotations) {
+                console.log('Annotations:');
+                console.log(annotations);
+            }
+            console.log('');
+            if (sender) {
+                // If we aren't a sender, no value to look for.
+                if (payload.DataValue === msgVal) {
+                    client.disconnect(function () {
+                        console.log("Disconnected, when we saw the value we'd inserted.");
+                        process.exit(0);
+                    });
+                }
+            }
+        }
+    };
+
+    var sendCB = function (tx_err, state) {
         if (tx_err) {
             console.log('Error Sending: ');
             console.log(tx_err);
         } else {
             console.log('State: ', state);
-            client.receive(uri, function (rx_err, payload, annotations) {
-                if (rx_err) {
-                    console.log('Error Receiving: ');
-                    console.log(rx_err);
-                } else {
-                    console.log('Recv: ');
-                    console.log(payload);
-                    if (annotations) {
-                        console.log('Annotations:');
-                        console.log(annotations);
-                    }
-                    console.log('');
-                    if (payload.DataValue === msgVal) {
-                        client.disconnect(function () {
-                            console.log("Disconnected, when we saw the value we'd inserted.");
-                        });
-                    }
-                }
-            });
+            if (receiver) {
+                client.receive(uri, receiveCB);
+            } else {
+                console.log('Sent message with value ' + msgVal + '.  Not receiving, so exiting');
+                process.exit(0);
+            }
         }
-    });
+    };
+
+    var client = new AMQPClient(AMQPClient.policies.ServiceBusQueuePolicy);
+    if (sender) {
+        client.send({"DataString": "From Node", "DataValue": msgVal}, uri, sendCB);
+    } else if (receiver) {
+        client.receive(uri, receiveCB);
+    }
 }
 
