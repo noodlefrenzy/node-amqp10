d12313d7a87aa6cd97b1a6d10e84e8c844bc625a
Matt Broadstone <mbroadst@gmail.com>
2015-08-03 16:49:29 -0400

    feat(default-subject): allow specifying subjects in link names
    This patch adds support for defining a subject in the name of a
    link during link creation. On the receiver side this adds a filter
    for the subject, and on the sender side this adds a default subject
    to the message properties (if one does not currently exist).

---
+++ test/integration/qpid/receiver_link.test.js
@@ -0,0 +1,46 @@
+'use strict';
+var AMQPClient = require('../../..').Client,
+    c = require('../../../').Constants,
+    Promise = require('bluebird'),
+    config = require('./config'),
+    expect = require('chai').expect;
+
+var test = {};
+describe('QPID', function() {
+
+describe('ReceiverLink', function() {
+  beforeEach(function() {
+    if (!!test.client) test.client = undefined;
+    test.client = new AMQPClient();
+  });
+
+  afterEach(function() {
+    return test.client.disconnect().then(function() {
+      test.client = undefined;
+    });
+  });
+
+  it('should allow the definition of a default subject', function(done) {
+    return test.client.connect(config.address)
+      .then(function() {
+        return Promise.all([
+          test.client.createReceiver('amq.topic/not-news'),
+          test.client.createReceiver('amq.topic/news'),
+          test.client.createSender('amq.topic/news')
+        ]);
+      })
+      .spread(function(receiverWithoutSubject, receiverWithSubject, sender) {
+        receiverWithoutSubject.on('message', function(message) {
+          expect(message).to.not.exist;
+        });
+
+        receiverWithSubject.on('message', function(message) {
+          done();
+        });
+
+        return sender.send('test message', { properties: { subject: 'news' } });
+      });
+  });
+
+});
+});
