b1feb7755c727245cd03ae7f800a8162ffe44bea
Matt Broadstone <mbroadst@gmail.com>
2015-08-04 13:26:15 -0400

    refactor(sb-example): show my version of refactoring the sb example

--- examples/simple_servicebus_topics.js
+++ examples/simple_servicebus_topics.js
@@ -14,7 +14,8 @@
 'use strict';
 //var AMQPClient = require('amqp10').Client;
 var AMQPClient  = require('../lib').Client,
-    Policy = require('../lib').Policy;
+    Policy = require('../lib').Policy,
+    util = require('util');
 
 // Simple argument-checker, you can ignore.
 function argCheck(settings, options) {
@@ -28,58 +29,63 @@ function argCheck(settings, options) {
 }
 
 if (process.argv.length < 3) {
-    console.warn('Usage: node '+process.argv[1]+' <settings json file>');
-} else {
-  var settingsFile = process.argv[2];
-  var settings = require('./' + settingsFile);
-  argCheck(settings, ['serviceBusHost', 'SASKeyName', 'SASKey', 'topicName', 'subscriptionName']);
-  var protocol = settings.protocol || 'amqps';
-  var serviceBusHost = settings.serviceBusHost + '.servicebus.windows.net';
-  if (settings.serviceBusHost.indexOf(".") !== -1) {
-    serviceBusHost = settings.serviceBusHost;
-  }
-  var sasName = settings.SASKeyName;
-  var sasKey = settings.SASKey;
-  var topicName = settings.topicName;
-  var subscriptionName = settings.subscriptionName;
-
-  var msgVal = Math.floor(Math.random() * 10000);
-
-  var uri = protocol + '://' + encodeURIComponent(sasName) + ':' + encodeURIComponent(sasKey) + '@' + serviceBusHost;
-
-  var client = new AMQPClient(Policy.ServiceBusTopic);
-  client.connect(uri).then(function () {
-    client.createSender(topicName).then(function (sender) {
-      sender.on('errorReceived', function(tx_err) {
-        console.warn('===> TX ERROR: ', tx_err);
-      });
-      client.createReceiver(topicName + '/Subscriptions/' + subscriptionName).then(function (receiver) {
-        receiver.on('message', function(message) {
-          console.log('Recv: ');
-          console.log(message.body);
-          if (message.annotations) {
-            console.log('Annotations:');
-            console.log(message.annotations);
-          }
-          console.log('');
-          if (message.body.DataValue === msgVal) {
-            client.disconnect().then(function () {
-              console.log("Disconnected, when we saw the value we'd inserted.");
-              process.exit(0);
-            });
-          }
-        });
-        receiver.on('errorReceived', function(rx_err) {
-          console.warn('===> RX ERROR: ', rx_err);
-        });
-        sender.send({"DataString": "From Node", "DataValue": msgVal}).then(function (state) {
-          console.log('State: ', state);
-        });
-      })
-    });
-  }).catch(function (e) {
-    console.warn('Error send/receive: ', e);
-  });
+  console.warn('Usage: node '+process.argv[1]+' <settings json file>');
+  return process.exit(0);
 }
 
+var settingsFile = process.argv[2];
+var settings = require('./' + settingsFile);
+argCheck(settings, ['serviceBusHost', 'SASKeyName', 'SASKey', 'topicName', 'subscriptionName']);
 
+var protocol = settings.protocol || 'amqps';
+var serviceBusHost = settings.serviceBusHost + '.servicebus.windows.net';
+if (settings.serviceBusHost.indexOf(".") !== -1) {
+  serviceBusHost = settings.serviceBusHost;
+}
+
+var sasName = settings.SASKeyName;
+var sasKey = settings.SASKey;
+var topicName = settings.topicName;
+var subscriptionName = settings.subscriptionName;
+
+var msgVal = Math.floor(Math.random() * 10000);
+var uri = protocol + '://' + encodeURIComponent(sasName) + ':' + encodeURIComponent(sasKey) + '@' + serviceBusHost;
+var client = new AMQPClient(Policy.ServiceBusTopic);
+
+client.connect(uri)
+  .then(function () {
+    return Promise.all([
+      client.createSender(topicName),
+      client.createReceiver(topicName + '/Subscriptions/' + subscriptionName)
+    ]);
+  })
+  .spread(function(sender, receiver) {
+    // error handling
+    sender.on('errorReceived', function(tx_err) {
+      console.warn('===> TX ERROR: ', tx_err);
+    });
+    receiver.on('errorReceived', function(rx_err) {
+      console.warn('===> RX ERROR: ', rx_err);
+    });
+
+    // message event handler
+    receiver.on('message', function(message) {
+      console.log('Recv: ' + util.inspect(message));
+
+      if (message.body.DataValue === msgVal) {
+        client.disconnect().then(function () {
+          console.log("Disconnected, when we saw the value we'd inserted.");
+          process.exit(0);
+        });
+      }
+    });
+
+    // send test message
+    return sender.send({"DataString": "From Node", "DataValue": msgVal});
+  })
+  .then(function(state) {
+    console.log('State: ', state);
+  })
+  .catch(function (e) {
+    console.warn('Error send/receive: ', e);
+  });
