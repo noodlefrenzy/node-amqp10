66cb519b5e328b4c628cb5ede8f07eaf39e2301a
Mike Lanzetta <milanz@microsoft.com>
2015-08-05 21:54:45 -0700

    Fix link and session reattach logic to avoid handle collisions, update reattach/reconnect example to exercise both attach and connect retries.

--- examples/servicebus_reconnect.js
+++ examples/servicebus_reconnect.js
@@ -1,8 +1,19 @@
+///
+/// A simple example showing ServiceBus' disconnect behavior:
+/// - It seems to drop unused send links at around 10 minutes, despite heartbeat frames.
+/// - It then drops the connection entirely at around 20 minutes.
+///
+/// The Link has logic to deal with re-attaching for this detach at 10 minutes, and
+///  the connection has logic to deal with re-connecting and re-establishing links.
+///
+/// The code below demonstrates both.
+///
 
 'use strict';
 //var AMQPClient = require('amqp10').Client;
 var AMQPClient  = require('../lib').Client,
-  Policy = require('../lib').Policy;
+  Policy = require('../lib').Policy,
+  Promise = require('bluebird');
 
 // Simple argument-checker, you can ignore.
 function argCheck(settings, options) {
@@ -34,48 +45,80 @@ var queueName = settings.queueName;
 
 var uri = protocol + '://' + encodeURIComponent(sasName) + ':' + encodeURIComponent(sasKey) + '@' + serviceBusHost;
 
-var msgVal = 1;
-var delayBetweenSending = 1000 * 60;
-var iteration = 0;
-var numIters = 16;
-var client = new AMQPClient(Policy.ServiceBusQueue);
-client.connect(uri).then(function () {
-  client.on('client:errorReceived', function (err) {
-    console.warn('===> CLIENT ERROR: ', err);
+function sendReceiveAndQuit(policy, minutesUntilSend) {
+  var msgVal = Math.floor(Math.random() * 1000000);
+  var min = 60 * 1000;
+  var remaining = minutesUntilSend;
+  return new Promise(function (resolve, reject) {
+    var client = new AMQPClient(policy);
+    client.on('client:errorReceived', function (err) {
+      console.warn('===> CLIENT ERROR: ', err);
+    });
+    client.on('connection:closed', function() {
+      console.warn('===> CONNECTION CLOSED');
+    });
+    client.connect(uri).then(function() {
+      return client.createSender(queueName);
+    })
+      .then(function (sender) {
+        sender.on('errorReceived', function(tx_err) {
+          console.warn('===> TX ERROR: ', tx_err);
+        });
+        sender.on('detached', function() {
+          console.warn('===> TX DETACHED');
+        });
+        sender.session.on('unmapped', function() {
+          console.warn('===> SESSION UNMAPPED');
+        });
+        sender.session.on('errorReceived', function(err) {
+          console.warn('===> SESSION ERROR: ', err);
+        });
+        setTimeout(function() {
+          client.disconnect().then(function() {
+            reject('Message not seen in ' + (minutesUntilSend + 1) + ' minutes');
+          });
+        }, (minutesUntilSend + 1) * min);
+        var interval = setInterval(function() {
+          remaining--;
+          if (remaining === 0) {
+            clearInterval(interval);
+            client.createReceiver(queueName).then(function (receiver) {
+              receiver.on('errorReceived', function (rx_err) {
+                console.warn('===> RX ERROR: ', rx_err);
+              });
+              receiver.on('detached', function () {
+                console.warn('===> RX DETACHED');
+              });
+              receiver.on('message', function (msg) {
+                if (msg.body && msg.body.DataValue && msg.body.DataValue === msgVal) {
+                  client.disconnect().then(function () {
+                    console.log("\n=================\nExpected Value Seen\n==================\n");
+                    resolve();
+                  });
+                }
+              });
+              sender.send({"DataString": "From Node", "DataValue": msgVal}).then(function (state) {
+                console.log('State: ', state);
+              }).catch(function (eSend) {
+                console.warn('===> TX SEND FAILURE: ', eSend);
+              });
+            });
+          } else {
+            console.log('===> ' + remaining + ' minutes remaining until sending.');
+          }
+        }, min);
+      })
   });
-  client.on('connection:closed', function() {
-    console.warn('===> CONNECTION CLOSED');
+}
+
+var noReattach = Policy.merge({ senderLink: { reattach: null }, receiverLink: { reattach: null } }, Policy.ServiceBusQueue);
+var reattach = Policy.ServiceBusQueue;
+
+var minutesUntilServiceBusDropsLink = 10;
+var minutesUntilServiceBusDropsConnection = 20;
+sendReceiveAndQuit(reattach, minutesUntilServiceBusDropsLink+ 1).then(function() {
+  sendReceiveAndQuit(noReattach, minutesUntilServiceBusDropsConnection + 1).then(function() {
+    console.log("\n==================\nComplete\n==================");
   });
-  return client.createSender(queueName);
-}).then(function (sender) {
-  sender.on('errorReceived', function(tx_err) {
-    console.warn('===> TX ERROR: ', tx_err);
-  });
-  sender.on('detached', function() {
-    console.warn('===> TX DETACHED');
-  });
-  sender.session.on('unmapped', function() {
-    console.warn('===> SESSION UNMAPPED');
-  });
-  sender.session.on('errorReceived', function(err) {
-    console.warn('===> SESSION ERROR: ', err);
-  });
-  setInterval(function () {
-    iteration++;
-    if (iteration === numIters) {
-      numIters /= 2;
-      iteration = 0;
-      console.log('===> SENDING message ' + msgVal + ', ' + numIters + ' mins to next send. Link state: ' + sender.state());
-      sender.send({"DataString": "From Node", "DataValue": msgVal++}).then(function (state) {
-        console.log('State: ', state);
-      }).catch(function (eSend) {
-        console.warn('===> TX SEND FAILURE: ', eSend);
-      });
-    } else {
-      console.log('===> ' + (numIters - iteration) + ' minutes until sending. Link state: ' + sender.state());
-    }
-  }, delayBetweenSending);
-}).catch(function (e) {
-  console.warn('===> CONNECT ERROR: ', e);
 });
 
