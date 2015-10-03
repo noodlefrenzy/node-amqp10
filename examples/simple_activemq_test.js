8ae12f67d9d2b002631bb075d978ed3f50b08435
Matt Broadstone <mbroadst@gmail.com>
2015-10-03 09:28:57 -0400

    refactor(activemq-test): refactor simple test to use updated API

--- examples/simple_activemq_test.js
+++ examples/simple_activemq_test.js
@@ -1,44 +1,35 @@
 'use strict';
 //var AMQPClient = require('amqp10').Client;
-var AMQPClient  = require('../lib').Client;
+var AMQPClient  = require('../lib').Client,
+    Promise = require('bluebird');
 
-var uri = 'amqp://some.host/random2';
-var msgId = Math.floor(Math.random() * 10000);
+var uri = 'amqp://some.host',
+    msgId = Math.floor(Math.random() * 10000),
+    client = new AMQPClient();
 
-var client = new AMQPClient();
-client.connect(uri).then(function () {
-    var msgStr = JSON.stringify({"DataString": "From Node", "DataValue": msgId});
-    console.log('Sending ' + msgStr);
-    client.send(msgStr).then(function () {
-        console.log('Sent');
-    }).catch(function (e) {
-        console.log('Error Sending ' + e);
+client.connect(uri)
+  .then(function () {
+    return Promise.all([
+      client.createReceiver('amq.topic'),
+      client.createSender('amq.topic')
+    ]);
+  })
+  .spread(function(receiver, sender) {
+    receiver.on('errorReceived', function(err) { console.log('error receiving: ', err); });
+    receiver.on('message', function(message) {
+      console.log('received: ', message.body);
+      if (message.annotations) console.log('annotations: ', message.annotations);
+      if (message.body.dataValue === msgId) {
+        client.disconnect().then(function() {
+          console.log("received expected message, disconnected.");
+        });
+      }
     });
-}).then(function() {
-    client.createReceiver(function (err, message) {
-        if (err) {
-            console.log('Error Receiving ' + err);
-        } else {
-            console.log('Recv: ');
-            console.log(message.body);
-            if (message.annotations) {
-                console.log('Annotations:');
-                console.log(message.annotations);
-            }
-            console.log('');
-            try {
-                var asJson = JSON.parse(message.body);
-                if (asJson.DataValue === msgId) {
-                    client.disconnect(function () {
-                        console.log("Received expected message.  Disconnected.");
-                    });
-                }
-            } catch (e) {
-                console.log("Error parsing payload " + message.body);
-                console.log(e);
-            }
-        }
-    });
-}).catch(function (e) {
-    console.log('Error Connecting: ' + e);
-});
+
+    var message = { dataString: "From Node", dataValue: msgId };
+    console.log('sending: ', message);
+    return sender.send(message);
+  })
+  .catch(function (e) {
+    console.log('connection error: ', e);
+  });
