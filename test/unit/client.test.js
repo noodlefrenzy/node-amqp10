2f32d03aeb36e2640aa0338bcba3a5794824ecc4
Mike Lanzetta <milanz@microsoft.com>
2015-11-03 15:25:16 -0800

    Add failing test for multi-frame message receipt.

--- test/unit/client.test.js
+++ test/unit/client.test.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var expect = require('chai').expect,
+    BufferList = require('bl'),
+    Builder = require('buffer-builder'),
     AMQPClient = require('../../lib').Client,
     MockServer = require('./mocks').Server,
 
@@ -16,6 +18,7 @@ var expect = require('chai').expect,
     DefaultPolicy = require('../../lib/policies/default_policy'),
     AMQPError = require('../../lib/types/amqp_error'),
     M = require('../../lib/types/message'),
+    Codec = require('../../lib/codec'),
 
     test = require('./test-fixture');
 
@@ -40,7 +43,7 @@ describe('Client', function() {
         });
     });
 
-    it('should connect then disconnect', function() {
+    it('should connect then disconnect', function(done) {
       test.server.setResponseSequence([
         constants.amqpVersion,
         new OpenFrame(DefaultPolicy.connect.options),
@@ -52,11 +55,13 @@ describe('Client', function() {
 
       return test.client.connect(test.server.address())
         .then(function() {
-          return test.client.disconnect();
+          return test.client.disconnect().then(function() {
+            done();
+          });
         });
     });
 
-    it('should connect and receive', function() {
+    it('should connect and receive', function(done) {
       test.server.setResponseSequence([
         constants.amqpVersion,
         new OpenFrame(DefaultPolicy.connect.options),
@@ -87,10 +92,62 @@ describe('Client', function() {
           rxLink.on('message', function (msg) {
             expect(msg.body).not.to.be.null;
             expect(msg.body.test).to.eql('testing');
-            test.client.disconnect();
+            test.client.disconnect().then(function() {
+              done();
+            });
           });
         });
     });
 
+    it('should receive multi-frame messages', function(done) {
+      var message = new M.Message({}, { test: 'testing' });
+      var tmpBuf = new Builder();
+      Codec.encode(message, tmpBuf);
+      var messageBuf = tmpBuf.get();
+      var buf1 = messageBuf.slice(0, 10);
+      var buf2 = messageBuf.slice(10);
+      test.server.setResponseSequence([
+        constants.amqpVersion,
+        new OpenFrame(DefaultPolicy.connect.options),
+        new BeginFrame({
+          remoteChannel: 1, nextOutgoingId: 0, incomingWindow: 2147483647, outgoingWindow: 2147483647, handleMax: 4294967295
+        }),
+        function (prev) {
+          var rxAttach = FrameReader.read(prev[prev.length-1]);
+          return new AttachFrame({
+            name: rxAttach.name, handle: 1, role: constants.linkRole.sender, source: {}, target: {}, initialDeliveryCount: 0
+          });
+        },
+        function (prev) {
+          var txFrame = new TransferFrame({
+            handle: 1
+          });
+          txFrame.message = buf1;
+          return txFrame;
+        },
+        function (prev) {
+          var txFrame = new TransferFrame({
+            handle: 1
+          });
+          txFrame.message = buf2;
+          return txFrame;
+        },
+        new CloseFrame(new AMQPError(AMQPError.ConnectionForced, 'test'))
+      ]);
+
+      return test.client.connect(test.server.address())
+        .then(function() {
+          return test.client.createReceiver('testing');
+        })
+        .then(function (rxLink) {
+          rxLink.on('message', function (msg) {
+            expect(msg.body).not.to.be.null;
+            expect(msg.body.test).to.eql('testing');
+            test.client.disconnect().then(function() {
+              done();
+            });
+          });
+        });
+    });
   });
 });
