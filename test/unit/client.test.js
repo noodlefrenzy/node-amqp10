a8f3eeb166e2fc61c4f767fab5159d373716f7c9
Matt Broadstone <mbroadst@gmail.com>
2015-11-03 21:29:57 -0500

    test(Transfer): add basic test for sending a transfer frame

--- test/unit/client.test.js
+++ test/unit/client.test.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var expect = require('chai').expect,
-    BufferList = require('bl'),
     Builder = require('buffer-builder'),
     AMQPClient = require('../../lib').Client,
     MockServer = require('./mocks').Server,
@@ -12,8 +11,13 @@ var expect = require('chai').expect,
     OpenFrame = require('../../lib/frames/open_frame'),
     BeginFrame = require('../../lib/frames/begin_frame'),
     AttachFrame = require('../../lib/frames/attach_frame'),
+    FlowFrame = require('../../lib/frames/flow_frame'),
     TransferFrame = require('../../lib/frames/transfer_frame'),
     CloseFrame = require('../../lib/frames/close_frame'),
+    DispositionFrame = require('../../lib/frames/disposition_frame'),
+
+    M = require('../../lib/types/message'),
+    DeliveryState = require('../../lib/types/delivery_state'),
 
     DefaultPolicy = require('../../lib/policies/default_policy'),
     AMQPError = require('../../lib/types/amqp_error'),
@@ -23,6 +27,8 @@ var expect = require('chai').expect,
     test = require('./test-fixture');
 
 DefaultPolicy.connect.options.containerId = 'test';
+DefaultPolicy.reconnect.retries = 0;
+DefaultPolicy.reconnect.forever = false;
 
 describe('Client', function() {
   describe('#connect()', function() {
@@ -106,6 +112,7 @@ describe('Client', function() {
       var messageBuf = tmpBuf.get();
       var buf1 = messageBuf.slice(0, 10);
       var buf2 = messageBuf.slice(10);
+
       test.server.setResponseSequence([
         constants.amqpVersion,
         new OpenFrame(DefaultPolicy.connect.options),
@@ -151,5 +158,55 @@ describe('Client', function() {
           });
         });
     });
+
+    it('should send multi-frame messages', function() {
+      test.server.setResponseSequence([
+        constants.amqpVersion,
+        new OpenFrame(DefaultPolicy.connect.options),
+        new BeginFrame({
+          remoteChannel: 1, nextOutgoingId: 0, incomingWindow: 100000,
+          outgoingWindow: 2147483647, handleMax: 4294967295
+        }),
+        [
+          function (prev) {
+            var rxAttach = FrameReader.read(prev[prev.length-1]);
+            return new AttachFrame({
+              name: rxAttach.name, handle: 1, role: constants.linkRole.receiver,
+              source: {}, target: {}, initialDeliveryCount: 0, maxMessageSize: 1 // <-- the important part
+            });
+          },
+          new FlowFrame({
+            handle: 1, deliveryCount: 1,
+            nextIncomingId: 1, incomingWindow: 2147483647,
+            nextOutgoingId: 0, outgoingWindow: 2147483647,
+            linkCredit: 500
+          })
+        ],
+        new DispositionFrame({
+          role: constants.linkRole.receiver, first: 1, last: 1, settled: true, batchable: false,
+          state: new DeliveryState.Accepted()
+        }),
+        new CloseFrame(new AMQPError(AMQPError.ConnectionForced, 'test'))
+      ]);
+
+      test.server.setExpectedFrameSequence([
+        false, false, false, false,
+        new TransferFrame({
+          frameType: 0, channel: 1, handle: 0, deliveryId: 1, settled: false, deliveryTag: 1,
+          message: new M.Message({ body: 'supercalifragilisticexpialidocious' }),
+          messageFormat: 0, more: false, receiverSettleMode: null,
+          state: null, resume: false, aborted: false, batchable: false
+        }),
+        false
+      ]);
+
+      return test.client.connect(test.server.address())
+        .then(function() { return test.client.createSender('test.link'); })
+        .then(function(sender) { return sender.send('supercalifragilisticexpialidocious'); })
+        .then(function() { return test.client.disconnect(); });
+    });
+
+
+
   });
 });
