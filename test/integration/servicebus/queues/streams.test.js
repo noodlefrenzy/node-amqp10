6dd8e50ba56efb2cf2361faaf8304db8ab620c37
Matt Broadstone <mbroadst@gmail.com>
2016-03-01 10:10:15 -0500

    test(servicebus): check for DataValue, update timeout

--- test/integration/servicebus/queues/streams.test.js
+++ test/integration/servicebus/queues/streams.test.js
@@ -3,7 +3,8 @@ var Promise = require('bluebird'),
     AMQPClient = require('../../../..').Client,
     Policy = require('../../../..').Policy,
     config = require('./config'),
-    expect = require('chai').expect;
+    expect = require('chai').expect,
+    uuid = require('uuid');
 
 var test = {};
 function setup() {
@@ -25,8 +26,9 @@ describe('ServiceBus', function() {
     afterEach(teardown);
 
     it('should let you create a receiver link as a readable stream', function(done) {
-      var expected = Array.apply(null, new Array(100))
-        .map(function(a) { return Math.floor(Math.random() * 100); });
+      var dataString = uuid.v4().replace(/-/g, ''),
+          expected = Array.apply(null, new Array(100))
+            .map(function(a) { return Math.floor(Math.random() * 100); });
 
       return Promise.all([
         test.client.createReceiverStream(config.defaultLink),
@@ -35,12 +37,15 @@ describe('ServiceBus', function() {
       .spread(function(stream, sender) {
         var count = 0;
         stream.on('data', function(data) {
-          expect(expected[count]).to.eql(data.body);
+          if (data.body.DataString !== dataString) return;  // ignore previously run tests
+          expect(expected[count]).to.eql(data.body.DataValue);
           count++;
           if (count === expected.length) done();
         });
 
-        return Promise.mapSeries(expected, function(v) { return sender.send(v); });
+        return Promise.mapSeries(expected, function(v) {
+          return sender.send({ DataString: dataString, DataValue: v });
+        });
       });
     });
   }); // ReceiverStream
@@ -49,9 +54,10 @@ describe('ServiceBus', function() {
     beforeEach(setup);
     afterEach(teardown);
 
-    it('should let you create a receiver link as a readable stream', function(done) {
-      var expected = Array.apply(null, new Array(100))
-        .map(function(a) { return Math.floor(Math.random() * 100); });
+    it('should let you create a sender link as a writable stream', function(done) {
+      var dataString = uuid.v4().replace(/-/g, ''),
+          expected = Array.apply(null, new Array(100))
+            .map(function(a) { return Math.floor(Math.random() * 100); });
 
       return Promise.all([
         test.client.createReceiver(config.defaultLink),
@@ -60,46 +66,18 @@ describe('ServiceBus', function() {
       .spread(function(receiver, stream) {
         var count = 0;
         receiver.on('message', function(data) {
-          expect(expected[count]).to.eql(data.body);
+          if (data.body.DataString !== dataString) return;  // ignore previously run tests
+          expect(expected[count]).to.eql(data.body.DataValue);
           count++;
           if (count === expected.length) done();
         });
 
         for (var i = 0; i < expected.length; i++) {
-          stream.write(expected[i]);
+          stream.write({ DataString: dataString, DataValue: expected[i] });
         }
       });
     });
   }); // SenderStream
-
-  describe('Both', function() {
-    beforeEach(setup);
-    afterEach(teardown);
-    it('should allow you to stream from sender to receiver', function(done) {
-      var expected = Array.apply(null, new Array(100))
-        .map(function(a) { return Math.floor(Math.random() * 100); });
-
-      return Promise.all([
-        test.client.createReceiver(config.defaultLink),
-        test.client.createReceiverStream('test.streams.queue'),
-        test.client.createSenderStream(config.defaultLink),
-      ])
-      .spread(function(receiver, receiverStream, senderStream) {
-        var count = 0;
-        receiver.on('message', function(message) {
-          expect(expected[count]).to.eql(message.body);
-          count++;
-          if (count === expected.length) done();
-        });
-
-        receiverStream.pipe(senderStream);
-        return test.client.createSender('test.streams.queue');
-      })
-      .then(function(sender) {
-        return Promise.mapSeries(expected, function(v) { return sender.send(v); });
-      });
-    });
-  }); // Both
 
   }); // Streams
 
