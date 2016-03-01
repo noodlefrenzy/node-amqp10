1ce32eaf96d335d1418fd1eaaef9eb5cad46a1eb
Matt Broadstone <mbroadst@gmail.com>
2016-03-01 09:47:33 -0500

    test(streams): properly name streams tests

--- test/integration/qpid/streams.test.js
+++ test/integration/qpid/streams.test.js
@@ -16,110 +16,114 @@ function teardown() {
     .then(function() { delete test.client; });
 }
 
-describe('Streams', function() {
-describe('ReceiverStream', function() {
-  beforeEach(setup);
-  afterEach(teardown);
+describe('QPID', function() {
 
-  it('should let you create a receiver link as a readable stream', function(done) {
-    var expected = Array.apply(null, new Array(100))
-      .map(function(a) { return Math.floor(Math.random() * 100); });
+  describe('Streams', function() {
+  describe('ReceiverStream', function() {
+    beforeEach(setup);
+    afterEach(teardown);
 
-    return Promise.all([
-      test.client.createReceiverStream(config.defaultLink),
-      test.client.createSender(config.defaultLink)
-    ])
-    .spread(function(stream, sender) {
-      var count = 0;
-      stream.on('data', function(data) {
-        expect(expected[count]).to.eql(data.body);
-        count++;
-        if (count === expected.length) done();
+    it('should let you create a receiver link as a readable stream', function(done) {
+      var expected = Array.apply(null, new Array(100))
+        .map(function(a) { return Math.floor(Math.random() * 100); });
+
+      return Promise.all([
+        test.client.createReceiverStream(config.defaultLink),
+        test.client.createSender(config.defaultLink)
+      ])
+      .spread(function(stream, sender) {
+        var count = 0;
+        stream.on('data', function(data) {
+          expect(expected[count]).to.eql(data.body);
+          count++;
+          if (count === expected.length) done();
+        });
+
+        return Promise.mapSeries(expected, function(v) { return sender.send(v); });
       });
-
-      return Promise.mapSeries(expected, function(v) { return sender.send(v); });
     });
-  });
 
-}); // ReceiverStream
+  }); // ReceiverStream
 
-describe('SenderStream', function() {
-  beforeEach(setup);
-  afterEach(teardown);
+  describe('SenderStream', function() {
+    beforeEach(setup);
+    afterEach(teardown);
 
-  it('should let you create a sender link as a writable stream', function(done) {
-    var expected = Array.apply(null, new Array(100))
-      .map(function(a) { return Math.floor(Math.random() * 100); });
+    it('should let you create a sender link as a writable stream', function(done) {
+      var expected = Array.apply(null, new Array(100))
+        .map(function(a) { return Math.floor(Math.random() * 100); });
 
-    return Promise.all([
-      test.client.createReceiver(config.defaultLink),
-      test.client.createSenderStream(config.defaultLink)
-    ])
-    .spread(function(receiver, stream) {
-      var count = 0;
-      receiver.on('message', function(data) {
-        expect(expected[count]).to.eql(data.body);
-        count++;
-        if (count === expected.length) done();
+      return Promise.all([
+        test.client.createReceiver(config.defaultLink),
+        test.client.createSenderStream(config.defaultLink)
+      ])
+      .spread(function(receiver, stream) {
+        var count = 0;
+        receiver.on('message', function(data) {
+          expect(expected[count]).to.eql(data.body);
+          count++;
+          if (count === expected.length) done();
+        });
+
+        for (var i = 0; i < expected.length; ++i) {
+          stream.write(expected[i]);
+        }
       });
-
-      for (var i = 0; i < expected.length; ++i) {
-        stream.write(expected[i]);
-      }
     });
-  });
 
-  it('should honor the sender link callback policy', function(done) {
-    var expected = Array.apply(null, new Array(100))
-      .map(function(a) { return Math.floor(Math.random() * 100); });
+    it('should honor the sender link callback policy', function(done) {
+      var expected = Array.apply(null, new Array(100))
+        .map(function(a) { return Math.floor(Math.random() * 100); });
 
-    return Promise.all([
-      test.client.createReceiver(config.defaultLink),
-      test.client.createSenderStream(config.defaultLink, { callback: 'sent' })
-    ])
-    .spread(function(receiver, stream) {
-      var count = 0;
-      receiver.on('message', function(data) {
-        expect(expected[count]).to.eql(data.body);
-        count++;
-        if (count === expected.length) done();
+      return Promise.all([
+        test.client.createReceiver(config.defaultLink),
+        test.client.createSenderStream(config.defaultLink, { callback: 'sent' })
+      ])
+      .spread(function(receiver, stream) {
+        var count = 0;
+        receiver.on('message', function(data) {
+          expect(expected[count]).to.eql(data.body);
+          count++;
+          if (count === expected.length) done();
+        });
+
+        for (var i = 0; i < expected.length; ++i) {
+          stream.write(expected[i]);
+        }
       });
-
-      for (var i = 0; i < expected.length; ++i) {
-        stream.write(expected[i]);
-      }
     });
-  });
 
-}); // SenderStream
+  }); // SenderStream
 
-describe('Both', function() {
-  beforeEach(setup);
-  afterEach(teardown);
-  it('should allow you to stream from sender to receiver', function(done) {
-    var expected = Array.apply(null, new Array(100))
-      .map(function(a) { return Math.floor(Math.random() * 100); });
+  describe('Both', function() {
+    beforeEach(setup);
+    afterEach(teardown);
+    it('should allow you to stream from sender to receiver', function(done) {
+      var expected = Array.apply(null, new Array(100))
+        .map(function(a) { return Math.floor(Math.random() * 100); });
 
-    return Promise.all([
-      test.client.createReceiver(config.defaultLink),
-      test.client.createReceiverStream('test.streams.queue'),
-      test.client.createSenderStream(config.defaultLink),
-    ])
-    .spread(function(receiver, receiverStream, senderStream) {
-      var count = 0;
-      receiver.on('message', function(message) {
-        expect(expected[count]).to.eql(message.body);
-        count++;
-        if (count === expected.length) done();
+      return Promise.all([
+        test.client.createReceiver(config.defaultLink),
+        test.client.createReceiverStream('test.streams.queue'),
+        test.client.createSenderStream(config.defaultLink),
+      ])
+      .spread(function(receiver, receiverStream, senderStream) {
+        var count = 0;
+        receiver.on('message', function(message) {
+          expect(expected[count]).to.eql(message.body);
+          count++;
+          if (count === expected.length) done();
+        });
+
+        receiverStream.pipe(senderStream);
+        return test.client.createSender('test.streams.queue');
+      })
+      .then(function(sender) {
+        return Promise.mapSeries(expected, function(v) { return sender.send(v); });
       });
-
-      receiverStream.pipe(senderStream);
-      return test.client.createSender('test.streams.queue');
-    })
-    .then(function(sender) {
-      return Promise.mapSeries(expected, function(v) { return sender.send(v); });
     });
-  });
-}); // Both
+  }); // Both
 
-}); // Streams
+  }); // Streams
+
+}); // QPID
