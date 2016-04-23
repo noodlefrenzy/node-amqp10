6270331c4d83f32d186ab4c52f080d10125603fb
Matt Broadstone <mbroadst@gmail.com>
2016-04-23 12:32:48 -0400

    test(release): show RenewOnSettle working with release outcomes

--- test/integration/qpid/receiver_link.test.js
+++ test/integration/qpid/receiver_link.test.js
@@ -1,12 +1,12 @@
 'use strict';
 var AMQPClient = require('../../..').Client,
+    Policy = require('../../..').Policy,
     Promise = require('bluebird'),
     config = require('./config'),
     expect = require('chai').expect;
 
 var test = {};
 describe('QPID', function() {
-
 describe('ReceiverLink', function() {
   beforeEach(function() {
     if (!!test.client) test.client = undefined;
@@ -41,5 +41,39 @@ describe('ReceiverLink', function() {
       });
   });
 
-});
-});
+  it('should refresh link after settling with `release` outcome', function(done) {
+    var throttledClient =
+      new AMQPClient(Policy.Utils.RenewOnSettle(1, 1, Policy.Default));
+
+    return Promise.all([
+      test.client.connect(config.address),
+      throttledClient.connect(config.address)
+    ])
+    .then(function() {
+      return Promise.all([
+        test.client.createSender('amq.topic'),
+        throttledClient.createReceiver('amq.topic')
+      ]);
+    })
+    .spread(function(sender, receiver) {
+      var messages = ['test1', 'test2', 'test3'];
+      var received = 0;
+      receiver.on('message', function(msg) {
+        expect(msg.body).to.eql(messages[received]);
+        received++;
+        this.release(msg);
+        if (received === messages.length) {
+          throttledClient.disconnect().then(done);
+        }
+      });
+
+      return Promise.all([
+        sender.send(messages[0]),
+        sender.send(messages[1]),
+        sender.send(messages[2])
+      ]);
+    });
+  });
+
+}); // ReceiverLink
+}); // QPID
